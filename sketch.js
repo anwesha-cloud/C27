f24784566fcc1fb96459a67ed00ba9bd3bb33679
anwesha-cloud <anweshamaitin2005@gmail.com>
2020-05-02 13:00:50 +0530

    constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,8 +36,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    clog= new Log(200,100,40,PI);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,clog.body)
 
 }
 
@@ -59,8 +63,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    clog.display();
 
     bird.display();
     platform.display();
     platform1.display();
+    chain.display();
 }
